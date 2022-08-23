9b4fe190b843a0d3ab3c029ee38d74fc6f6fa700
Zoraz Haseeb <78059639+Zoraz01@users.noreply.github.com>
2022-08-23 12:12:06 -0400

    update

--- cmsc401.java
+++ cmsc401.java
@@ -1,21 +1,23 @@
-Scanner sc = new Scanner(System.in);
-int loopNumber = sc.nextInt();
-int index = 0;
-int [] answers = new int[loopNumber];
+public static void main(String args){
+    Scanner sc = new Scanner(System.in);
+    int loopNumber = sc.nextInt();
+    int index = 0;
+    int [] answers = new int[loopNumber];
 
-for(int i =0; i < loopNumber;i++){
-    int secondLoop = sc.nextInt;
-    int [] arr = new int [secondLoop - 1];
-    for(int j =1; j < secondLoop -1;j++){
-        arr[j] = sc.nextInt;
+    for(int i =0; i < loopNumber;i++){
+        int secondLoop = sc.nextInt;
+        int [] arr = new int [secondLoop - 1];
+        for(int j =1; j < secondLoop -1;j++){
+            arr[j] = sc.nextInt;
 
+        }
+        int firstIndex = sc.nextInt;
+        int secondIndex = sc.nextInt;
+        int ans = arr[firstIndex] + arr[secondIndex];
+        answers[index] = ans;
+        index++;
     }
-    int firstIndex = sc.nextInt;
-    int secondIndex = sc.nextInt;
-    int ans = arr[firstIndex] + arr[secondIndex];
-    answers[index] = ans;
-    index++;
-}
-for(int i =0; i < loopNumber;i++){
-    System.out.print(answers[i]);
+    for(int i =0; i < loopNumber;i++){
+        System.out.print(answers[i]);
+    }
 }
