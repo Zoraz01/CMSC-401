b43fc8b7015cefb3527d98002398610fef55d58f
Zoraz Haseeb <78059639+Zoraz01@users.noreply.github.com>
2022-09-01 11:29:19 -0400

    update

--- majorityElement.java
+++ majorityElement.java
@@ -1,1 +1,14 @@
+//Zoraz Haseeb
+class mahorityElement{
+    public static void main(String[] args){
 
+    }
+    public int getMajority(int [] arr){
+        int majority = 0;
+        int majCounter = 0;
+        for(int i =0; i < arr.length; i++){
+            majority = arr[i];
+
+        }
+    }
+}
