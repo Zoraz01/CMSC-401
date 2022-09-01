516290c80d3cd6e0056feb00f3dfe902775eb884
Zoraz Haseeb <78059639+Zoraz01@users.noreply.github.com>
2022-09-01 11:40:36 -0400

    Further work

--- majorityElement.java
+++ majorityElement.java
@@ -5,10 +5,20 @@ class majorityElement{
     }
     public int getMajority(int [] arr){
         int majority = 0;
-        int majCounter = 0;
-        for(int i =0; i < arr.length; i++){
-            majority = arr[i];
+        int majCounter = 1;
+        majority = arr[i];
 
+        for(int i =1; i < arr.length; i++){
+            if(majority == arr[i]){
+                majCounter++;
+            }
+            else{
+                majCounter--;
+            }
+            if(majCounter == 0){
+                majority = arr[i+1];
+                majCounter = 1;
+            }
         }
     }
 }
