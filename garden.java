3dc9fd115d7d9e56f0f7925ba765403d64e2c539
Zoraz Haseeb <78059639+Zoraz01@users.noreply.github.com>
2022-09-29 12:48:47 -0400

    progress

--- garden.java
+++ garden.java
@@ -1,20 +1,26 @@
-
+import java.util.Scanner;
+import java.lang.Math;
 class garden{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int numberOfGardens = sc.nextInt;
+        int numberOfGardens = sc.nextInt();
         int [] y = new int[numberOfGardens];
         int [] size = new int[numberOfGardens];
-        int maxY = Math.MIN_INTEGER;
-        int minY = Math.MAX_INTEGER; 
+        int sum1 = 0; 
+        int sum2 = 0;
         for(int i =0; i < numberOfGardens; i++){
-            y[i] = sc.nextInt;
-            size[i] = sc.nextInt;
-            maxY
+            y[i] = sc.nextInt();
+            size[i] = sc.nextInt();
+            sum1 = sum1 + size[i] * size[i] * y[i];
+            sum2 = sum2 + size[i] * size[i];
         }
-
-    }
-    public int getOptimalPosition(int [] y, int [] size, int maxY,int minY){
+        int x =  sum1 / sum2;
+        double totalCost = 0;
+        for(int i =0; i < numberOfGardens;i++){
+            totalCost = totalCost + Math.abs(x - y[i] * size[i]);
+        }
+        System.out.println((int) x);
+        System.out.println((int)totalCost);
 
     }
 
